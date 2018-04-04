a32061bca6a31de4dc1b8d28373698788f187c75
Natasha Desai <tashadesai@gmail.com>
2018-04-04 15:35:53 -0400

    updated resume link

--- browser/components/Resume.jsx
+++ browser/components/Resume.jsx
@@ -8,8 +8,8 @@ class Resume extends Component {
           <img id="resumeTitle" className="pt5 titleImg" src='/layer-images/resume.png' />
         </div>
         <div className="w-100-ns center ph6-ns">
-          <p><a className="pa3-ns" href="/other-assets/NatashaDesai18¸.pdf">Download my resume</a></p>
-          <embed id="embededPDF" className="fl w-100" type="application/pdf" src="/other-assets/NatashaDesai2018.pdf" width="640" height="480" />
+          <p><a className="pa3-ns" href="/other-assets/NatashaDesai18.pdf">Download my resume</a></p>
+          <embed id="embededPDF" className="fl w-100" type="application/pdf" src="/other-assets/NatashaDesai18.pdf" width="640" height="480" />
           </div>
       </div>
     );
